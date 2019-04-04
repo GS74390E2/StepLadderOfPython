5f0c1d5add3ed77b54236e6dc445d1140b7b3f0a
gejiayu <gejiayu@baidu.com>
2019-04-04 22:48:30 +0800

    update django learning

--- Web_Django/DailyFresh/apps/goods/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from apps.goods import views
-
-app_name = 'goods'
-urlpatterns = [
-    path('index', views.index, name='index'),
-]
--- Web_Django/env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/gs74390e2/StepLadderOfPython/Web_Django/dfenv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
